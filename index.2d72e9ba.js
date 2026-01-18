79b7e4634f291b1ffa558b081c91f06533115626
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-18 23:56:26 +0100

    make build

---
+++ index.2d72e9ba.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=document.querySelector("img#largeImg");e.addEventListener("click",function(e){var t=e.target.closest("a");t&&(e.preventDefault(),r.src=t.href)});
+//# sourceMappingURL=index.2d72e9ba.js.map
